e974ae4196c42e7f145f74aae6271c3965b179d1
Rick <tenhsor@mac-rick.local>
2017-10-15 21:52:23 -0500

    Added ueberauth packages and config

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,17 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] },
+    facebook: { Ueberauth.Strategy.Facebook, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: System.get_env("ELIXIR_GITHUB_API_ID"),
+  client_secret: System.get_env("ELIXIR_GITHUB_API_SECRET")
+
+config :ueberauth, Ueberauth.Strategy.Facebook.OAuth,
+client_id: System.get_env("ELIXIR_FACEBOOK_API_ID"),
+client_secret: System.get_env("ELIXIR_FACEBOOK_API_SECRET")
