6b4efceb63b4799ba5312af41e398099a158dfb8
Rick <tenhsor@mac-rick.local>
2017-12-17 21:23:49 -0600

    Authenticated sockets

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -1,6 +1,6 @@
 defmodule Discuss.UserSocket do
   use Phoenix.Socket
-
+  alias Discuss.User
   ## Channels
   channel "comments:*", Discuss.CommentsChannel
 
@@ -19,8 +19,13 @@ defmodule Discuss.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket) do
-    {:ok, socket}
+  def connect(%{"token" => token}, socket) do
+    case Phoenix.Token.verify(socket, "key", token) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+      {:error, _err} ->
+        :error
+    end
   end
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
